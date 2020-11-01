b49a3f920bc8261a2d4c97cb0eb8f6f59b4cf689
Irina Stepanova <irina.s.vesna@mail.ru>
2020-11-01 06:53:05 +0600

    feat: add vigenere-cipher task solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,41 @@
-const CustomError = require("../extensions/custom-error");
-
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor (direction) {
+    this.direction = direction;
+    this.alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  }
+
+  encrypt(message, key) {
+    if (!message || !key) throw new Error('Enter required parameters!');
+    let res = [];
+    let messageLow = message.toLowerCase();
+    let keyLow = key.toLowerCase();
+    let j = 0;
+    for (let i = 0; i < messageLow.length; i++) {
+      if (this.alphabet.indexOf(messageLow.charAt(i)) !== -1) {
+        res.push(this.alphabet.charAt((this.alphabet.indexOf(messageLow.charAt(i)) + this.alphabet.indexOf(keyLow.charAt(j % key.length))) % this.alphabet.length));
+        j++;
+      } else {
+        res.push(messageLow.charAt(i));
+      }
+    }
+    return this.direction !== 'false' ? res.join('').toUpperCase() : res.reverse().join('').toUpperCase();
+  }
+
+  decrypt(message, key) {
+    if (!message || !key) throw new Error('Enter required parameters!');
+    let res = [];
+    let messageLow = message.toLowerCase();
+    let keyLow = key.toLowerCase();
+    let j = 0;
+    for (let i = 0; i < messageLow.length; i++) {
+      if (this.alphabet.indexOf(messageLow.charAt(i)) !== -1) {
+        res.push(this.alphabet.charAt((this.alphabet.indexOf(messageLow.charAt(i)) + this.alphabet.length - this.alphabet.indexOf(keyLow.charAt(j % key.length))) % this.alphabet.length));
+        j++;
+      } else {
+        res.push(messageLow.charAt(i));
+      }
+    }
+    return this.direction !== 'false' ? res.join('').toUpperCase() : res.reverse().join('').toUpperCase();
   }
 }
 
