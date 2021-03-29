a73af691a254241ba62ad670a03a49d565fcb57c
arumirinka <68826013+arumirinka@users.noreply.github.com>
2021-03-30 04:23:02 +0600

    fix: carbon-dating task solution

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -2,6 +2,6 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity) {
-  const res = Math.ceil(Math.log2(MODERN_ACTIVITY / sampleActivity) / (0.693 / HALF_LIFE_PERIOD));
+  const res = Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) / (0.693 / HALF_LIFE_PERIOD));
   return (!sampleActivity || typeof sampleActivity !== 'string' || sampleActivity > MODERN_ACTIVITY || !isFinite(res)) ? false : res;
 };
